ddbfb10fc0e5671ad11ec90e154f9a3546439134
lisa814 <elisfro08@gmail.com>
2024-02-17 13:42:12 +0200

    added the robot's length and width

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -84,7 +84,9 @@ public final class Constants {
   }
 
   public static final class DriveConstants {
-    // Driving Parameters - Note that these are not the maximum capable speeds of
+	public static final double ROBOT_LENGTH =30;
+	public static final double ROBOT_WIDTH =30;
+	// Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
     public static final double kMaxSpeedMetersPerSecond = 4.8;
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
