e9695704f961eda536674d10015c36450998e8b7
Itay Zenkin <itayzenkin11@gmail.com>
2024-03-18 22:08:16 +0200

    Added MoveToKnownDegree

---
+++ src/main/java/frc/robot/commands/MoveToKnownDegree.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.LimeLight;
+import frc.robot.subsystems.Arm;
+
+public class MoveToKnownDegree extends Command {
+  private Arm arm;
+  private double output;
+  public MoveToKnownDegree(Arm arm) {
+    this.arm = arm;
+    addRequirements(arm);
+}
+  
+  @Override
+  public void initialize() {
+    arm.setSetpoint(26);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    arm.setSpeedByMeasurement(arm.getPosition());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    arm.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return arm.atSetpoint() || (arm.getPosition() < 3 && output < 0);
+  }
+}
